6b85c4a79a1d025e89e165a61b174af351a51bef
latchumnaiduy <latchumnaidu.yanda@xcubelabs.com>
2023-11-29 19:07:41 +0530

    new project

--- src/context/appcontext.js
+++
@@ -1,3 +0,0 @@
-"use client"
-import { createContext } from "react";
-export const appctx=createContext();
--- src/utils/init.js
+++
@@ -1,5 +0,0 @@
-export const init={
- isLoggedIn:false,
- isShowLoader:false,
- user:{}
-}
